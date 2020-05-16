8e94ed1bcd9244a99a9e95e1edca03cd2eef126b
Nick Enchev <nikolaenchev@gmail.com>
2020-05-16 15:08:09 -0400

    Loaded Asset

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,69 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <rapidjson/document.h>
 
 using namespace rapidjson;
 
+namespace gltf
+{
+	struct Asset
+	{
+		std::string version, generator, copyright;
+	};
+
+	struct Node
+	{
+	};
+
+	struct Scene
+	{
+		Scene();
+	};
+
+	struct Mesh
+	{
+		Mesh();
+	};
+
+	struct Model
+	{
+		Asset asset;
+	};
+}
+
+std::string getString(const Value &value, const std::string &key)
+{
+	if (value.HasMember(key.c_str()))
+	{
+		return std::string(value[key.c_str()].GetString());
+	}
+	else
+	{
+		return "";
+	}
+}
+
 int main()
 {
-	Document document;
-    return 0;
+  std::ifstream t("models/Box.gltf");
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+
+  Document document;
+  document.Parse(buffer.str().c_str());
+
+
+  using namespace gltf;
+  Model model;
+
+  // parse asset
+  assert(document.HasMember("asset"));
+  const Value &assetValue = document["asset"];
+  model.asset.version = getString(assetValue, "version");
+  model.asset.generator = getString(assetValue, "generator");
+  model.asset.copyright = getString(assetValue, "copyright");
+
+  return 0;
 }
